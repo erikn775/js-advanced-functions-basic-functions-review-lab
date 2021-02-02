be9bbab00539faf4b15c1f2c23f5f5ca1ab837c8
erikn775 <erikn775@gmail.com>
2021-02-01 23:35:59 -0700

    commit tree cd4aa8b858ffadd1a8469a7fe810b38e85855e35

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Your code here
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+function mondayWork(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+}
+
+function wrapAdjective(ad = "*") {
+    return function(first = "special") {
+        return `You are ${ad}${first}${ad}!`;
+    }
+}
+wrapAdjective("!!")("Cute Dog")
+
+const Calculator = {
+    add: function add(num1, num2) {
+        return num1 + num2;
+    },
+    subtract: function subtract(num1, num2) {
+        return num1 - num2;
+    },
+    multiply: function multiply(num1, num2) {
+        return num1 * num2;
+    },
+    divide: function divide(num1, num2) {
+        return num1 / num2;
+    }
+};
+
+
+function actionApplyer(a, array) {
+    if (array === undefined || array.length == 0) {
+        return a
+    }
+    else {
+        return (a * 2 + 1000) % 7
+    }
+}
